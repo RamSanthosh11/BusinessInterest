ae4e912c12855451a09c7269146061d668996094
unknown <ramu3317@gmail.com>
2020-02-23 10:29:34 +0530

    first commit

---
+++ src/main/java/IntrestCaluclator/IntresrCalc/BusinessHome.java
@@ -0,0 +1,8 @@
+package IntrestCaluclator.IntresrCalc;
+
+public class BusinessHome extends ClientApplication{
+
+    public int construction_cost(int n,int area) {
+        return (n*area);
+    }
+}
